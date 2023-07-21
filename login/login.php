c04f4437c9d38559edeb561b76031caf6c7845e7
Your Name <aaratirai777@gmail,com>
2023-07-21 12:57:33 +0545

    php connect

--- login/login.php
+++ login/login.php
@@ -9,6 +9,19 @@
 </head>
 <body>
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
 
 
     <div class="ss">
